33111105a3ec4b2bf850e66dd6a7688bb196cf14
Renzo Tomlinson <rttomlinson@gmail.com>
2017-04-22 18:29:52 +0000

    Clean up debugging logs

--- routes/admin.js
+++ routes/admin.js
@@ -10,19 +10,13 @@ router.get('/orders', function(req, res, next) {
                 orders
             });
         })
-        .catch((err) => {
-            console.log("An error occurred while getting data");
-            console.log(err.message);
-            console.log(err.stack);
-            next(err);
-        });
+        .catch(next);
 });
 
 router.get('/order/:id', function(req, res, next) {
     let id = req.params.id;
     h.getOrder(id)
         .then((orderInfo) => {
-            console.log("orderInfo", orderInfo);
             res.render('admin/show', {
                 orderInfo
             });
@@ -31,8 +25,6 @@ router.get('/order/:id', function(req, res, next) {
 });
 
 router.get('/analytics', function(req, res, next) {
-    console.log("inside router for analytics");
-
     getAnalyticsData()
         .then((totalsTable) => {
             res.render('admin/analytics', {
@@ -44,43 +36,20 @@ router.get('/analytics', function(req, res, next) {
 
 module.exports = router;
 
-
-
 async function getAnalyticsData() {
-    console.log("calling getAnalyticsData");
     //Query to get totals of all orders
     let totalsTable = {};
     //Query for total revenue
     totalsTable.totalRevenue = await h.getTotalRevenue();
-    console.log("calling totalREvenue");
-
     totalsTable.totalUnitsSold = await h.getUnitsSold();
-    console.log("calling unitsSold");
-
     totalsTable.totalTransactions = await h.getTotalTransactions();
-    console.log("calling totalTransactions");
-
     totalsTable.totalCustomers = await h.getTotalCustomers();
-    console.log("calling totalCustomer");
-
     totalsTable.totalProducts = await h.getTotalProducts();
-    console.log("calling totalProducts");
-
     totalsTable.totalCategories = await h.getTotalCategories();
-    console.log("calling totalCategories");
-
     totalsTable.statesSoldTo = await h.getStatesSoldTo();
-    console.log("calling getStatesSoldTo");
-
     totalsTable.revenueByProduct = await h.getRevenueByProduct();
-    console.log("calling getRevenueByProduct");
-
     totalsTable.revenueByState = await h.getRevenueByState();
-    console.log("calling getRevenueByState");
-
     totalsTable.revenueByCategory = await h.getRevenueByCategory();
-    console.log("calling getRevenueByCategory");
-
 
     return totalsTable;
 
