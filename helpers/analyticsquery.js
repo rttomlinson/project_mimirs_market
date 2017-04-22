d824647083826b271e589b39e8ecc1da8155249c
Renzo Tomlinson <rttomlinson@gmail.com>
2017-04-22 07:51:48 +0000

    Add async/await and helper functions for analytics router

---
+++ helpers/analyticsquery.js
@@ -0,0 +1,222 @@
+"use strict";
+
+let mongooseModels = require('../models/mongoose');
+let sequelizeModels = require('../models/sequelize');
+let Product = sequelizeModels.Product;
+let Category = sequelizeModels.Category;
+let Order = mongooseModels.Order;
+let moneyHelper = require('../helpers/money');
+let checkoutHelper = require('../helpers/checkout');
+
+
+
+let AnalyticsHelper = {};
+
+AnalyticsHelper.getOrders = async function() {
+    let orders = await Order.find({
+        completed: true
+    });
+    console.log("orders", orders);
+    orders = orders.map((order) => {
+        let tempObj = {};
+        tempObj._id = order._id;
+        tempObj.checkoutDate = order.paymentInfo.checkoutDate;
+        tempObj.description = checkoutHelper.itemNames(order.orderItems);
+        tempObj.revenue = moneyHelper.USDollars(order.paymentInfo.totalCharge);
+        tempObj.customerEmail = order.billingInfo.email;
+        tempObj.customerState = order.billingInfo.address.state;
+        return tempObj;
+    });
+    return orders;
+};
+
+AnalyticsHelper.getOrder = async function(id) {
+    let orders = await Order.findById(id, {});
+    return orders;
+};
+
+
+AnalyticsHelper.getTotalRevenue = async function() {
+    let orderInfo = await Order.aggregate([{
+        $match: {
+            completed: true
+        }
+    }, {
+        $group: {
+            _id: null,
+            total: {
+                $sum: '$paymentInfo.totalCharge'
+            }
+        }
+    }]);
+    return orderInfo[0].total;
+};
+
+AnalyticsHelper.getUnitsSold = async function() {
+    let orderInfo = await Order.mapReduce({
+        map: function() {
+            //loop through orderItems and grab quantities
+            let keys = Object.keys(this.orderItems);
+            let totalUnits = keys.reduce((acc, key) => {
+                return acc += this.orderItems[key].quantity;
+            }, 0);
+            emit(null, totalUnits);
+        },
+        reduce: function(keys, values) {
+            return Array.sum(values);
+        }
+    });
+    console.log("orderInfo", orderInfo);
+    return orderInfo[0].value;
+};
+
+
+AnalyticsHelper.getTotalTransactions = async function() {
+    let orderInfo = await Order.mapReduce({
+        map: function() {
+            //loop through orderItems and grab quantities
+            emit(null, 1);
+        },
+        reduce: function(keys, values) {
+            return Array.sum(values);
+        },
+        query: {
+            completed: true
+        }
+    });
+    return orderInfo[0].value;
+};
+
+
+AnalyticsHelper.getTotalCustomers = async function() {
+    let orderInfo = await Order.aggregate([{
+        $group: {
+            _id: '$billingInfo.email',
+            total: {
+                $sum: 1
+            }
+        }
+    }, {
+        $group: {
+            _id: null,
+            total: {
+                $sum: 1
+            }
+        }
+    }]);
+    return orderInfo[0].total;
+};
+
+AnalyticsHelper.getTotalProducts = async function() {
+    return await Product.count({});
+};
+
+AnalyticsHelper.getTotalCategories = async function() {
+    return await Category.count({});
+};
+
+AnalyticsHelper.getStatesSoldTo = async function() {
+    let statesArray = await Order.distinct(
+        "billingInfo.address.state", {});
+    return statesArray.length;
+};
+
+AnalyticsHelper.getRevenueByProduct = async function() {
+    let revenueByProduct = await Order.aggregate([{
+        $unwind: "$orderItems"
+    }, {
+        $project: {
+            _id: 0,
+            "orderItems.name": 1,
+            "orderItems.price": 1,
+            "orderItems.quantity": 1
+        }
+    }, {
+        $group: {
+            _id: "$orderItems.name",
+            total: {
+                $sum: {
+                    $multiply: ["$orderItems.price", "$orderItems.quantity"]
+                }
+            }
+        }
+    }]);
+    return revenueByProduct.map((product) => {
+        return {
+            identifier: product._id,
+            total: product.total
+        };
+    });
+};
+
+
+AnalyticsHelper.getRevenueByState = async function() {
+    let revenueByState = await Order.aggregate([{
+        $unwind: "$orderItems"
+    }, {
+        $project: {
+            _id: 0,
+            "billingInfo.address.state": 1,
+            "orderItems.name": 1,
+            "orderItems.price": 1,
+            "orderItems.quantity": 1
+        }
+    }, {
+        $group: {
+            _id: "$billingInfo.address.state",
+            total: {
+                $sum: {
+                    $multiply: ["$orderItems.price", "$orderItems.quantity"]
+                }
+            }
+        }
+    }]);
+    return revenueByState.map((product) => {
+        return {
+            identifier: product._id,
+            total: product.total
+        };
+    });
+};
+
+AnalyticsHelper.getRevenueByCategory = async function() {
+    //Get all categories
+    let categories = await Category.findAll({
+        raw: true
+    });
+    //map category ids to category names. will be used at end
+    let categoriesObj = {};
+    categories.forEach((category) => {
+        categoriesObj[category.id] = category.name;
+    });
+
+    //Get revenue by category from orders
+    let revenueByCategory = await Order.aggregate([{
+        $unwind: "$orderItems"
+    }, {
+        $project: {
+            _id: 0,
+            "orderItems.price": 1,
+            "orderItems.quantity": 1,
+            "orderItems.category_id": 1
+        }
+    }, {
+        $group: {
+            _id: "$orderItems.category_id",
+            total: {
+                $sum: {
+                    $multiply: ["$orderItems.price", "$orderItems.quantity"]
+                }
+            }
+        }
+    }]);
+    //map to cateogory names
+    return revenueByCategory.map((category) => {
+        return {
+            //CategoryObj has names of category corresponding to its ID
+            identifier: categoriesObj[category._id],
+            total: category.total
+        };
+    });
+};
+module.exports = AnalyticsHelper;
--- routes/admin.js
+++ routes/admin.js
@@ -9,20 +9,8 @@ let Order = mongooseModels.Order;
 let h = require('../helpers').registered;
 
 router.get('/orders', function(req, res, next) {
-    Order.find({})
-        .then((orders) => {
-            orders = orders.map((order) => {
-                let tempObj = {};
-                tempObj._id = order._id;
-                tempObj.checkoutDate = order.paymentInfo.checkoutDate;
-                tempObj.description = h.itemNames(order.orderItems);
-                tempObj.revenue = h.USDollars(order.paymentInfo.totalCharge);
-                tempObj.customerEmail = order.billingInfo.email;
-                tempObj.customerState = order.billingInfo.address.state;
-                return tempObj;
-            });
-            return orders;
-        })
+
+    h.getOrders()
         .then((orders) => {
             res.render('admin/index', {
                 orders
@@ -33,7 +21,8 @@ router.get('/orders', function(req, res, next) {
 
 router.get('/order/:id', function(req, res, next) {
     let id = req.params.id;
-    Order.findById(id, {})
+
+    h.getOrder(id)
         .then((orderInfo) => {
             console.log("orderInfo", orderInfo);
             res.render('admin/show', {
@@ -46,208 +35,8 @@ router.get('/order/:id', function(req, res, next) {
 
 router.get('/analytics', function(req, res, next) {
 
-    //Query to get totals of all orders
-    let totalsTable = {};
-    //Query for total revenue
-    Order.aggregate([{
-            $group: {
-                _id: null,
-                total: {
-                    $sum: '$paymentInfo.totalCharge'
-                }
-            }
-        }])
-        .then((orderInfo) => {
-            totalsTable.totalRevenue = orderInfo[0].total;
-            //Query for total units sold
-            return Order.mapReduce({
-                map: function() {
-                    //loop through orderItems and grab quantities
-                    let keys = Object.keys(this.orderItems);
-                    let totalUnits = keys.reduce((acc, key) => {
-                        return acc += this.orderItems[key].quantity;
-                    }, 0);
-                    emit(null, totalUnits);
-                },
-                reduce: function(keys, values) {
-                    return Array.sum(values);
-                }
-            });
-
-        })
-        .then((totalUnits) => {
-            console.log("totalUnits", totalUnits);
-            totalsTable.totalUnitsSold = totalUnits[0].value;
-            //Total transactions/total orders
-            return Order.mapReduce({
-                map: function() {
-                    //loop through orderItems and grab quantities
-                    emit(null, 1);
-                },
-                reduce: function(keys, values) {
-                    return Array.sum(values);
-                },
-                query: {
-                    completed: true
-                }
-            });
-        })
-        .then((totalTransactions) => {
-            console.log("totalTransactions", totalTransactions);
-            totalsTable.totalTransactions = totalTransactions[0].value;
-            //Get total unique customers
-            return Order.aggregate([{
-                $group: {
-                    _id: '$billingInfo.email',
-                    total: {
-                        $sum: 1
-                    }
-                }
-            }, {
-                $group: {
-                    _id: null,
-                    total: {
-                        $sum: 1
-                    }
-                }
-            }]);
-        })
-        .then((customers) => {
-            console.log("customers", customers);
-            totalsTable.totalCustomers = customers[0].total;
-            //get total products
-            return Product.count({});
-
-        })
-        .then((totalProducts) => {
-            console.log("Total products", totalProducts);
-            totalsTable.totalProducts = totalProducts;
-            //Get total categories
-            return Category.count({});
-
-
-        })
-        .then((totalCategories) => {
-            console.log("total categories", totalCategories);
-            totalsTable.totalCategories = totalCategories;
-            //Get total states sold to
-            return Order.distinct(
-                "billingInfo.address.state", {});
-
-        })
-        .then((statesSoldTo) => {
-            console.log("distinct states", statesSoldTo.length);
-            totalsTable.statesSoldTo = statesSoldTo.length;
-
-            //Get revenue per product
-            return Order.aggregate([{
-                $unwind: "$orderItems"
-            }, {
-                $project: {
-                    _id: 0,
-                    "orderItems.name": 1,
-                    "orderItems.price": 1,
-                    "orderItems.quantity": 1
-                }
-            }, {
-                $group: {
-                    _id: "$orderItems.name",
-                    total: {
-                        $sum: {
-                            $multiply: ["$orderItems.price", "$orderItems.quantity"]
-                        }
-                    }
-                }
-            }]);
-        })
-        .then((revenueByProduct) => {
-            console.log("revenue by product", revenueByProduct);
-            revenueByProduct = revenueByProduct.map((product) => {
-                return {
-                    identifier: product._id,
-                    total: product.total
-                };
-            });
-            totalsTable.revenueByProduct = revenueByProduct;
-            //Get revenue by state
-            return Order.aggregate([{
-                $unwind: "$orderItems"
-            }, {
-                $project: {
-                    _id: 0,
-                    "billingInfo.address.state": 1,
-                    "orderItems.name": 1,
-                    "orderItems.price": 1,
-                    "orderItems.quantity": 1
-                }
-            }, {
-                $group: {
-                    _id: "$billingInfo.address.state",
-                    total: {
-                        $sum: {
-                            $multiply: ["$orderItems.price", "$orderItems.quantity"]
-                        }
-                    }
-                }
-            }]);
-        })
-        .then((revenueByState) => {
-            console.log("revenueByState", revenueByState);
-            revenueByState = revenueByState.map((product) => {
-                return {
-                    identifier: product._id,
-                    total: product.total
-                };
-            });
-            totalsTable.revenueByState = revenueByState;
-            //Get revenue by category
-            //First get the products with the category names
-            return Category.findAll({
-                    raw: true
-                })
-                .then(categories => {
-                    let categoryObj = {};
-                    categories.forEach((category) => {
-                        categoryObj[category.id] = category.name;
-                    });
-
-                    //Get revenue by category from orders
-                    return Order.aggregate([{
-                            $unwind: "$orderItems"
-                        }, {
-                            $project: {
-                                _id: 0,
-                                "orderItems.price": 1,
-                                "orderItems.quantity": 1,
-                                "orderItems.category_id": 1
-                            }
-                        }, {
-                            $group: {
-                                _id: "$orderItems.category_id",
-                                total: {
-                                    $sum: {
-                                        $multiply: ["$orderItems.price", "$orderItems.quantity"]
-                                    }
-                                }
-                            }
-                        }])
-                        .then((revenueByCategory) => {
-                            //map to products
-                            return revenueByCategory.map((category) => {
-                                return {
-                                    //CategoryObj has names of category corresponding to its ID
-                                    identifier: categoryObj[category._id],
-                                    total: category.total
-                                };
-                            });
-                        });
-                });
-        })
-        .then((revenueByCategory) => {
-            console.log("revenueByCategory", revenueByCategory);
-            totalsTable.revenueByCategory = revenueByCategory;
-        })
-        .then(() => {
+    getAnalyticsData()
+        .then((totalsTable) => {
             res.render('admin/analytics', {
                 totalsTable
             });
@@ -259,7 +48,24 @@ module.exports = router;
 
 
 
+async function getAnalyticsData() {
+    //Query to get totals of all orders
+    let totalsTable = {};
+    //Query for total revenue
+    totalsTable.totalRevenue = await h.getTotalRevenue();
+    totalsTable.totalUnitsSold = await h.getUnitsSold();
+    totalsTable.totalTransactions = await h.getTotalTransactions();
+    totalsTable.totalCustomers = await h.getTotalCustomers();
+    totalsTable.totalProducts = await h.getTotalProducts();
+    totalsTable.totalCategories = await h.getTotalCategories();
+    totalsTable.statesSoldTo = await h.getStatesSoldTo();
+    totalsTable.revenueByProduct = await h.getRevenueByProduct();
+    totalsTable.revenueByState = await h.getRevenueByState();
+    totalsTable.revenueByCategory = await h.getRevenueByCategory();
 
+    return totalsTable;
+
+};
 
 
 
